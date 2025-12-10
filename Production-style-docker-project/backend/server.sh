2cc7a808b2713289cf6a271074b26a54b4475cb5
Shruti089 <prachikashyap376@gmail.com>
2025-12-10 12:55:49 +0000

    Update project configuration and README

--- Production-style-docker-project/backend/server.sh
+++ Production-style-docker-project/backend/server.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-while true
-do
-  echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nShell Backend API 🚀" | nc -l -p 5000
+while true; do
+  echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nShell Backend API 🚀" | nc -l -p 5000 -q 1
 done
 
